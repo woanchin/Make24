9c1dae966d54c2642c904597075041423a57cdde
Woan Chin <woanchin.tee@gmail.com>
2016-02-09 15:13:10 +0800

    added corner cases

--- Make24/src/Make24.java
+++ Make24/src/Make24.java
@@ -5,17 +5,94 @@ public class Make24 {
 	double first, sec, third, fourth;
 	String answer;
 	
-	public void main(String arg0) {
+	public void main(String[] arg0) {
 		Scanner sc = new Scanner(System.in);
-		first = sc.nextDouble(); 
-		sec = sc.nextDouble();
-		third = sc.nextDouble();
-		fourth = sc.nextDouble();
+		first = (double) sc.nextDouble(); 
+		sec = (double) sc.nextDouble();
+		third = (double) sc.nextDouble();
+		fourth = (double) sc.nextDouble();
 		
 		System.out.print(answer);
 	}
 	
 	public String calculate(double a, double b, double c, double d) {
+		//corner cases
+		
+		//(2 brackets)
+		if ((a + b) * (c + d) == 24) answer += "( " + a + " + " + b + " ) * ( " + c + " + " + d + " )";
+		if ((a + b) * (c - d) == 24) answer += "( " + a + " + " + b + " ) * ( " + c + " - " + d + " )";
+		if ((a + b) * (c * d) == 24) answer += "( " + a + " + " + b + " ) * ( " + c + " * " + d + " )";
+		if ((a + b) * (c / d) == 24) answer += "( " + a + " + " + b + " ) * ( " + c + " / " + d + " )";
+		
+		if ((a - b) * (c + d) == 24) answer += "( " + a + " - " + b + " ) * ( " + c + " + " + d + " )";
+		if ((a - b) * (c - d) == 24) answer += "( " + a + " - " + b + " ) * ( " + c + " - " + d + " )";
+		if ((a - b) * (c * d) == 24) answer += "( " + a + " - " + b + " ) * ( " + c + " * " + d + " )";
+		if ((a - b) * (c / d) == 24) answer += "( " + a + " - " + b + " ) * ( " + c + " / " + d + " )";
+		
+		if ((a * b) * (c + d) == 24) answer += "( " + a + " * " + b + " ) * ( " + c + " + " + d + " )";
+		if ((a * b) * (c - d) == 24) answer += "( " + a + " * " + b + " ) * ( " + c + " - " + d + " )";
+		
+		if ((a / b) * (c + d) == 24) answer += "( " + a + " / " + b + " ) * ( " + c + " + " + d + " )";
+		if ((a / b) * (c - d) == 24) answer += "( " + a + " / " + b + " ) * ( " + c + " - " + d + " )";
+		
+		if ((a + b) / (c + d) == 24) answer += "( " + a + " + " + b + " ) / ( " + c + " + " + d + " )";
+		if ((a + b) / (c - d) == 24) answer += "( " + a + " + " + b + " ) / ( " + c + " - " + d + " )";
+		if ((a + b) / (c * d) == 24) answer += "( " + a + " + " + b + " ) / ( " + c + " * " + d + " )";
+		if ((a + b) / (c / d) == 24) answer += "( " + a + " + " + b + " ) / ( " + c + " / " + d + " )";
+		
+		if ((a - b) / (c + d) == 24) answer += "( " + a + " - " + b + " ) / ( " + c + " + " + d + " )";
+		if ((a - b) / (c - d) == 24) answer += "( " + a + " - " + b + " ) / ( " + c + " - " + d + " )";
+		if ((a - b) / (c * d) == 24) answer += "( " + a + " - " + b + " ) / ( " + c + " * " + d + " )";
+		if ((a - b) / (c / d) == 24) answer += "( " + a + " - " + b + " ) / ( " + c + " / " + d + " )";
+		
+		if ((a * b) / (c + d) == 24) answer += "( " + a + " * " + b + " ) / ( " + c + " + " + d + " )";
+		if ((a * b) / (c - d) == 24) answer += "( " + a + " * " + b + " ) / ( " + c + " - " + d + " )";
+		
+		if ((a / b) / (c + d) == 24) answer += "( " + a + " / " + b + " ) / ( " + c + " + " + d + " )";
+		if ((a / b) / (c - d) == 24) answer += "( " + a + " / " + b + " ) / ( " + c + " - " + d + " )";
+		
+		//(1 bracket = '+')
+		if ((a + (b + c) + d) == 24) answer += a + " + (" + b + " + " + c + ") + " + d;
+		if ((a + (b + c) - d) == 24) answer += a + " + (" + b + " + " + c + ") - " + d;
+		if ((a + (b + c) * d) == 24) answer += a + " + (" + b + " + " + c + ") * " + d;
+		if ((a + (b + c) / d) == 24) answer += a + " + (" + b + " + " + c + ") / " + d;
+		
+		if ((a - (b + c) + d) == 24) answer += a + " - (" + b + " + " + c + ") + " + d;
+		if ((a - (b + c) - d) == 24) answer += a + " - (" + b + " + " + c + ") - " + d;
+		if ((a - (b + c) * d) == 24) answer += a + " - (" + b + " + " + c + ") * " + d;
+		if ((a - (b + c) / d) == 24) answer += a + " - (" + b + " + " + c + ") / " + d;
+		
+		if ((a * (b + c) + d) == 24) answer += a + " * (" + b + " + " + c + ") + " + d;
+		if ((a * (b + c) - d) == 24) answer += a + " * (" + b + " + " + c + ") - " + d;
+		if ((a * (b + c) * d) == 24) answer += a + " * (" + b + " + " + c + ") * " + d;
+		if ((a * (b + c) / d) == 24) answer += a + " * (" + b + " + " + c + ") / " + d;
+		
+		if ((a / (b + c) + d) == 24) answer += a + " / (" + b + " + " + c + ") + " + d;
+		if ((a / (b + c) - d) == 24) answer += a + " / (" + b + " + " + c + ") - " + d;
+		if ((a / (b + c) * d) == 24) answer += a + " / (" + b + " + " + c + ") * " + d;
+		if ((a / (b + c) / d) == 24) answer += a + " / (" + b + " + " + c + ") / " + d;
+		
+		//(1 bracket = '-')
+		if ((a + (b - c) + d) == 24) answer += a + " + (" + b + " + " + c + ") + " + d;
+		if ((a + (b - c) - d) == 24) answer += a + " + (" + b + " + " + c + ") - " + d;
+		if ((a + (b - c) * d) == 24) answer += a + " + (" + b + " + " + c + ") * " + d;
+		if ((a + (b - c) / d) == 24) answer += a + " + (" + b + " + " + c + ") / " + d;
+		
+		if ((a - (b - c) + d) == 24) answer += a + " - (" + b + " + " + c + ") + " + d;
+		if ((a - (b - c) - d) == 24) answer += a + " - (" + b + " + " + c + ") - " + d;
+		if ((a - (b - c) * d) == 24) answer += a + " - (" + b + " + " + c + ") * " + d;
+		if ((a - (b - c) / d) == 24) answer += a + " - (" + b + " + " + c + ") / " + d;
+		
+		if ((a * (b - c) + d) == 24) answer += a + " * (" + b + " + " + c + ") + " + d;
+		if ((a * (b - c) - d) == 24) answer += a + " * (" + b + " + " + c + ") - " + d;
+		if ((a * (b - c) * d) == 24) answer += a + " * (" + b + " + " + c + ") * " + d;
+		if ((a * (b - c) / d) == 24) answer += a + " * (" + b + " + " + c + ") / " + d;
+		
+		if ((a / (b - c) + d) == 24) answer += a + " / (" + b + " + " + c + ") + " + d;
+		if ((a / (b - c) - d) == 24) answer += a + " / (" + b + " + " + c + ") - " + d;
+		if ((a / (b - c) * d) == 24) answer += a + " / (" + b + " + " + c + ") * " + d;
+		if ((a / (b - c) / d) == 24) answer += a + " / (" + b + " + " + c + ") / " + d;
+		
 		// addition
 		if (a + b + c + d == 24) answer += a + " + " + b + " + " + c + " + " + d + "\n";
 		if (a + b + c - d == 24) answer += a + " + " + b + " + " + c + " - " + d + "\n";
